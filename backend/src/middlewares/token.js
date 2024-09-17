dbe4e902400ed1eff44c4d69fd060193bfe73b7c
zuamaya <a01784238@tec.mx>
2024-09-17 17:22:36 -0600

    Feat: add middleware to protect one route

---
+++ backend/src/middlewares/token.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+export const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token)
+    {
+        return res.status(403).json({message: 'Token not provided' });
+    }
+
+    try
+    {
+        const bearerToken = token.split(' ')[1];
+
+        const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+    }
+    catch(error)
+    {
+        console.error('Invalid token:', error);
+        return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+}
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/users.js';
+import { verifyToken } from '../middlewares/token.js'
 
 export const usersRouter = Router();
 
-usersRouter.get('/get', UserController.getAll);
+usersRouter.get('/get', verifyToken,  UserController.getAll);
 usersRouter.post('/register', UserController.registerNewUser);
 usersRouter.post('/login', UserController.getLogin);
