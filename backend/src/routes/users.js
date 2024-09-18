739d727932962f15a7359caaa9a90d751869cf6a
zuamaya <a01784238@tec.mx>
2024-09-18 16:52:44 -0600

    feat: add token in each fetch of data from the frontend and verify in backend

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -5,13 +5,13 @@ import { verifyToken } from '../middlewares/token.js'
 export const usersRouter = Router();
 
 usersRouter.get('/get', verifyToken,  UserController.getAll);
-usersRouter.post('/register', UserController.registerNewUser);
+usersRouter.post('/register', verifyToken,  UserController.registerNewUser);
 
 // Iniciar sesión de un usuario
 usersRouter.post('/login', UserController.getLogin);
 
 // Actualizar un usuario existente
-usersRouter.put('/update/:id', UserController.updateUser);
+usersRouter.put('/update/:id', verifyToken,  UserController.updateUser);
 
 // Eliminar un usuario
-usersRouter.delete('/:id', UserController.deleteUser);
+usersRouter.delete('/:id', verifyToken, UserController.deleteUser);
