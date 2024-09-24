e439d5ed2e2637cc23b969964e6a83fbed47e07b
Lisette <lismelor@gmail.com>
2024-09-24 16:49:09 -0600

    add register page and connection in database

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,47 +1,70 @@
 "use client";
 
-import React, { useState, useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { createUser } from '../services/api'; // Asegúrate de que esta ruta sea la correcta
 
-const LoginPage = () => {
+const SignUp = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [phone, setPhone] = useState('');
   const [error, setError] = useState<string | null>(null); // Para manejar errores
-  const authContext = useContext(AuthContext);
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (authContext) {
-      try {
-        // Esperar la llamada de login
-        await authContext.login(email, password);
-      } catch (error) {
-        setError('Error en la autenticación. Inténtalo de nuevo.');
-        console.error('Error en la autenticación en front de page:', error);
-      }
+    if (password !== confirmPassword) {
+      setError('Las contraseñas no coinciden.');
+      return;
+    }
+
+    const userData = {
+      name,
+      email,
+      password,
+      phone,
+      role_id: 2, // Aquí puedes especificar el rol que corresponde. Esto depende de cómo manejes los roles en tu backend.
+    };
+
+    try {
+      await createUser(userData); // Llamamos a la función para crear el usuario
+      router.push('/login'); // Redirigimos al usuario al login después del registro
+    } catch (error) {
+      setError('Error en el registro. Inténtalo de nuevo.');
+      console.error('Error en el registro:', error);
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-no-repeat bg-cover bg-center">
-       <div
-  style={{
-    background: 'linear-gradient(to top right, #232959 50%, #1F244D 50%)',
-  }}
-  className="absolute top-0 left-0 w-full h-full"
-></div>
+      <div
+        style={{
+          background: 'linear-gradient(to top right, #232959 50%, #1F244D 50%)',
+        }}
+        className="absolute top-0 left-0 w-full h-full"
+      ></div>
       <div className="relative z-10 items-center justify-center w-full max-w-md bg-[rgba(255,255,255)] rounded-lg shadow-md p-6">
-        <div className="flex items-center justify-center ">
+        <div className="flex items-center justify-center">
           <img src="/images/logoAzul.png" alt="Logo Fundación Sanders" className="h-12 w-13" />
-          <h2 className="text-2xl font-semibold text-center text-[#202451] m-11 ">Login</h2>
+          <h2 className="text-2xl font-semibold text-center text-[#202451] m-11 ">Registro</h2>
         </div>
-        
 
         <form onSubmit={handleSubmit}>
           {error && <p className="text-red-500 mb-4">{error}</p>} {/* Mostrar el error si existe */}
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-[#202451]">Nombre</label>
+            <input
+              id="name"
+              type="text"
+              className="w-full px-4 py-2 border rounded-lg text-[#202451]"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
           <div className="mb-4">
             <label htmlFor="email" className="block text-[#202451]">Email</label>
             <input
@@ -53,8 +76,18 @@ const LoginPage = () => {
               required
             />
           </div>
+          <div className="mb-4">
+            <label htmlFor="phone" className="block text-[#202451]">Teléfono</label>
+            <input
+              id="phone"
+              type="tel"
+              className="w-full px-4 py-2 border rounded-lg text-[#202451]"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
           <div className="mb-4 relative">
-            <label htmlFor="password" className="block text-[#202451]">Password</label>
+            <label htmlFor="password" className="block text-[#202451]">Contraseña</label>
             <input
               id="password"
               type="password"
@@ -64,11 +97,27 @@ const LoginPage = () => {
               required
             />
           </div>
+          <div className="mb-4 relative">
+            <label htmlFor="confirmPassword" className="block text-[#202451]">Confirmar Contraseña</label>
+            <input
+              id="confirmPassword"
+              type="password"
+              className="w-full px-4 py-2 border rounded-lg text-gray-900"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-[#202451] text-white py-2 px-4 rounded-lg hover:bg-[#1f3b6d] transition-colors duration-300"
+          >
+            Registrarse
+          </button>
         </form>
-
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default SignUp;
